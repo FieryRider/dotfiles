ae87bc8c4cdd6e31cd21f218c0331d17e00c4a05
FieryRider <30357809+FieryRider@users.noreply.github.com>
2022-09-22 03:29:41 +0300

    Update conky config

--- .conkyrc_common.lua
+++ .conkyrc_common.lua
@@ -11,8 +11,8 @@ common_config = {
 	own_window_transparent = true,
 	own_window_hints = 'undecorated,below,sticky,skip_taskbar,skip_pager',
 	double_buffer = true,
-	minimum_width = 260, minimum_height = 5,
-	maximum_width = 300,
+	minimum_width = 415, minimum_height = 5,
+	maximum_width = 415,
 	draw_shades = false,
 	draw_outline = false,
 	draw_borders = false,
@@ -37,20 +37,23 @@ ${color #ffffff}${font Ubuntu mono:pixelsize=20,weight:Bold}System Info${font} $
 ${color #55aaff}$nodename${color} - $sysname $kernel
 ${color #55aaff}Uptime:${color} $uptime
 ${color #55aaff}Load:${color} $loadavg
-${color #55aaff}GPU: ${color}${exec lspci -d 10de:13c2 -k | command grep -Po '(?<=Kernel driver in use: ).*'} (${exec loginctl show-session $(loginctl | grep $(whoami) | awk '{print $1}') -p Type | grep -Po '(?<=Type=).*'})
+${color #55aaff}GPU: ${color}${execi 3600 lspci -d 10de:13c2 -k 2>/dev/null | command grep -Po '(?<=Kernel driver in use: ).*'} (${execi 3600 loginctl show-session $(loginctl | grep $(whoami) | awk '{print $1}') -p Type | cut -f2 -d'='})
 ${color #55aaff}Last Update: ${color}${execi 600 date -d $(grep --text "Running 'pacman -Syu'" /var/log/pacman.log | tail -1 | cut -d' ' -f1 | sed 's/[][]//g') '+%d-%m-%Y %H:%M:%S'}
 
 ${color #ffffff}${font Ubuntu mono:pixelsize=20,weight:Bold}CPU${font} ${hr 2}${color}
 
-${color #55aaff}CPU: ${color #dddddd}Intel Core i5-4670${color} @ ${freq_g} GHz
+${color #55aaff}CPU: ${color #dddddd}${execi 3600 grep 'model name' /proc/cpuinfo | uniq | cut -f2 -d':' | cut -c 2-}${color} @ ${freq_g} GHz ${alignr} ${font Font Awesome 6 Free:style=Solid:size=12}${if_match ${lua get_cpu_temp} < 35}${else}${if_match ${lua get_cpu_temp} < 60}${else}${endif}${endif}${font} ${color #ff0000}${lua get_cpu_temp}°C${color} 
 CPU utilization$alignr ${color #ffffff}${cpu cpu0}%${color}
 ${color #5555ff}${cpubar cpu0}${color}
-Core 1: ${color #5555ff}${cpubar cpu1 6,160}${color}  ${color #ffffff}${cpu cpu1}%${color}
-Core 2: ${color #5555ff}${cpubar cpu2 6,160}${color}  ${color #ffffff}${cpu cpu2}%${color}
-Core 3: ${color #5555ff}${cpubar cpu3 6,160}${color}  ${color #ffffff}${cpu cpu3}%${color}
-Core 4: ${color #5555ff}${cpubar cpu4 6,160}${color}  ${color #ffffff}${cpu cpu4}%${color}
+Thread 1:  ${color #5555ff}${cpubar cpu1 6,80}${color} ${color #ffffff}${cpu cpu1}%${color} ${goto 200}Thread 2:  ${color #5555ff}${cpubar cpu2 6,80}${color}  ${color #ffffff}${cpu cpu2}%${color}
+Thread 3:  ${color #5555ff}${cpubar cpu3 6,80}${color} ${color #ffffff}${cpu cpu3}%${color} ${goto 200}Thread 4:  ${color #5555ff}${cpubar cpu4 6,80}${color}  ${color #ffffff}${cpu cpu4}%${color}
+Thread 5:  ${color #5555ff}${cpubar cpu5 6,80}${color} ${color #ffffff}${cpu cpu6}%${color} ${goto 200}Thread 6:  ${color #5555ff}${cpubar cpu2 6,80}${color}  ${color #ffffff}${cpu cpu2}%${color}
+Thread 7:  ${color #5555ff}${cpubar cpu7 6,80}${color} ${color #ffffff}${cpu cpu8}%${color} ${goto 200}Thread 8:  ${color #5555ff}${cpubar cpu4 6,80}${color}  ${color #ffffff}${cpu cpu4}%${color}
+Thread 9:  ${color #5555ff}${cpubar cpu9 6,80}${color} ${color #ffffff}${cpu cpu10}%${color} ${goto 200}Thread 10: ${color #5555ff}${cpubar cpu4 6,80}${color}  ${color #ffffff}${cpu cpu4}%${color}
+Thread 11: ${color #5555ff}${cpubar cpu11 6,80}${color} ${color #ffffff}${cpu cpu12}%${color} ${goto 200}Thread 12: ${color #5555ff}${cpubar cpu4 6,80}${color}  ${color #ffffff}${cpu cpu4}%${color}
+Thread 13: ${color #5555ff}${cpubar cpu13 6,80}${color} ${color #ffffff}${cpu cpu14}%${color} ${goto 200}Thread 14: ${color #5555ff}${cpubar cpu4 6,80}${color}  ${color #ffffff}${cpu cpu4}%${color}
+Thread 15: ${color #5555ff}${cpubar cpu15 6,80}${color} ${color #ffffff}${cpu cpu16}%${color} ${goto 200}Thread 16: ${color #5555ff}${cpubar cpu4 6,80}${color}  ${color #ffffff}${cpu cpu4}%${color}
 IOWait: ${color #ffff00}${execi 1 iostat -c 1 1 | tail -4 | head -1 | awk '{print $4}'}%${color}
-${font Font Awesome 5 Free:style=Solid:size=12}${if_match ${lua get_cpu_temp 0} < 35}${else}${if_match ${lua get_cpu_temp 0} < 60}${else}${endif}${endif}${font} ${color #ff0000}${lua get_cpu_temp 0}°C | ${lua get_cpu_temp 1}°C ${lua get_cpu_temp 2}°C ${lua get_cpu_temp 3}°C ${lua get_cpu_temp 4}°C${color}
 
 ${color #ffffff}${font Ubuntu mono:pixelsize=20,weight:Bold}Memory${font} ${hr 2}${color}
 
@@ -64,7 +67,7 @@ ${color #ffffff}${font Ubuntu mono:pixelsize=20,weight:Bold}Top Processes${font}
 
 ${color #ffffff}$processes${color} processes
 
-${color #dddddd}${font Ubuntu mono:pixelsize=14,weight:Bold}NAME               PID     CPU     MEM${font}${color}
+${color #dddddd}${font Ubuntu mono:pixelsize=14,weight:Bold}NAME $alignr PID     CPU     MEM${font}${color}
 ${top name 1} $alignr ${top pid 1} ${top cpu 1}% ${top mem 1}%
 ${top name 2} $alignr ${top pid 2} ${top cpu 2}% ${top mem 2}%
 ${top name 3} $alignr ${top pid 3} ${top cpu 3}% ${top mem 3}%
